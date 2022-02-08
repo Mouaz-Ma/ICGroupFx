a415b8e266ba1df2ba811d85063a20cfc95c4c24
mouaz-m <maatouq.45@gmail.com>
2022-02-08 23:31:19 +0200

    login

--- controllers/users.js
+++ controllers/users.js
@@ -23,10 +23,34 @@ module.exports.renderLogin = (req, res) => {
 }
 
 module.exports.login = (req, res) => {
-    // req.flash('success', 'welcome back!');
-    // const redirectUrl = req.session.returnTo || '/';
-    // delete req.session.returnTo;
-    // res.redirect(redirectUrl);
+    if(!req.body.username){
+        res.json({success: false, message: "Username was not given"})
+      } else {
+        if(!req.body.password){
+          res.json({success: false, message: "Password was not given"})
+        }else{
+          passport.authenticate('local', function (err, user, info) { 
+             if(err){
+               res.json({success: false, message: err})
+             } else{
+              if (! user) {
+                res.json({success: false, message: 'username or password incorrect'})
+              } else{
+                req.login(user, function(err){
+                  if(err){
+                    res.json({success: false, message: err})
+                  }else{
+                    const token =  jwt.sign({userId : user._id, 
+                       username:user.username}, secretkey, 
+                          {expiresIn: '24h'})
+                    res.json({success:true, message:"Authentication successful", token: token });
+                  }
+                })
+              }
+             }
+          })(req, res);
+        }
+      }
 }
 
 module.exports.logout = (req, res) => {
