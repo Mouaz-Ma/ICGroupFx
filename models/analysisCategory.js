47283b0b76b2a95129317074e6982cbd866b4236
mouaz-ma <mouaz-m@Hotmail.com>
2022-04-27 16:14:31 +0300

    home page translation

--- models/analysisCategory.js
+++ models/analysisCategory.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AnalysisCategorySchema = new Schema({
-    type: { type: String, unique: true, required: true }
+    type: { 
+        type: String, 
+        unique: true, 
+        required: true 
+    },
+    ar:{ type: String }
+
 });
 
 module.exports = mongoose.model('analysisCategory', AnalysisCategorySchema);
